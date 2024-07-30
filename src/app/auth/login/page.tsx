769111cd1e191085fdb6c6c1b57ef651ed55c48d
zphelps <zach@zachphelps.com>
2024-07-29 21:51:24 -0400

    still have formAction error

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,32 @@
-import { login, signup } from './actions'
+"use client"
 import Link from "next/link";
+import {useAuth} from "@/hooks/useAuth";
+import {MouseEventHandler} from "react";
+import {useToast} from "@/components/ui/use-toast";
 
 export default function SignIn() {
+
+    const auth = useAuth();
+    const { toast } = useToast()
+
+    const handleSignInWithPassword= async (formData: FormData) => {
+        try {
+            const data = {
+                email: formData.get('email') as string,
+                password: formData.get('password') as string,
+            }
+            console.log(formData)
+            console.log(data)
+            await auth.signInWithEmailAndPassword(data);
+        } catch (error) {
+            console.log(error)
+            toast({
+                variant: "destructive",
+                title: "Uh oh! Something went wrong.",
+                description: "Make sure your email and password are correct and try again.",
+            })
+        }
+    }
 
     return (
         <section className="bg-gradient-to-b from-gray-100 to-white">
@@ -10,7 +35,7 @@ export default function SignIn() {
 
                     {/* Page header */}
                     <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                        <h1 className="h1">Welcome back.</h1>
+                        <h1 className="text-3xl font-bold">Welcome back.</h1>
                     </div>
 
                     {/* Form */}
@@ -19,7 +44,7 @@ export default function SignIn() {
                           <div className="flex flex-wrap -mx-3 mb-4">
                             <div className="w-full px-3">
                               <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email</label>
-                              <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
+                              <input id="email" type="email" className="p-3 rounded form-input w-full text-gray-800" placeholder="Enter your email address" required />
                             </div>
                           </div>
                           <div className="flex flex-wrap -mx-3 mb-4">
@@ -28,7 +53,7 @@ export default function SignIn() {
                                 <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Password</label>
                                 <Link href="/reset-password" className="text-sm font-medium text-blue-600 hover:underline">Having trouble signing in?</Link>
                               </div>
-                              <input id="password" type="password" className="form-input w-full text-gray-800" placeholder="Enter your password" required />
+                              <input id="password" type="password" className="p-3 rounded form-input w-full text-gray-800" placeholder="Enter your password" required />
                             </div>
                           </div>
                           <div className="flex flex-wrap -mx-3 mb-4">
@@ -43,7 +68,12 @@ export default function SignIn() {
                           </div>
                           <div className="flex flex-wrap -mx-3 mt-6">
                             <div className="w-full px-3">
-                              <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Sign in</button>
+                              <button
+                                  formAction={handleSignInWithPassword}
+                                  className="p-3 rounded btn text-white bg-blue-600 hover:bg-blue-700 w-full"
+                              >
+                                  Sign in
+                              </button>
                             </div>
                           </div>
                         </form>
@@ -52,11 +82,10 @@ export default function SignIn() {
                           <div className="text-gray-600 italic">Or</div>
                           <div className="border-t border-gray-300 grow ml-3" aria-hidden="true"></div>
                         </div>
-                        <form>
                         <div className="flex flex-wrap -mx-3 mb-3">
                           <div className="w-full px-3">
-                            <button className="btn px-0 text-white bg-gray-900 hover:bg-gray-800 w-full relative flex items-center">
-                              <svg className="w-4 h-4 fill-current text-white opacity-75 shrink-0 mx-4" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                            <button className="btn rounded p-3 text-white bg-gray-900 hover:bg-gray-800 w-full relative flex items-center">
+                              <svg className="w-6 h-6 fill-current text-white opacity-75 shrink-0 mx-4" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M7.95 0C3.578 0 0 3.578 0 7.95c0 3.479 2.286 6.46 5.466 7.553.397.1.497-.199.497-.397v-1.392c-2.187.497-2.683-.993-2.683-.993-.398-.895-.895-1.193-.895-1.193-.696-.497.1-.497.1-.497.795.1 1.192.795 1.192.795.696 1.292 1.888.895 2.286.696.1-.497.298-.895.497-1.093-1.79-.2-3.578-.895-3.578-3.975 0-.895.298-1.59.795-2.087-.1-.2-.397-.994.1-2.087 0 0 .695-.2 2.186.795a6.408 6.408 0 011.987-.299c.696 0 1.392.1 1.988.299 1.49-.994 2.186-.795 2.186-.795.398 1.093.199 1.888.1 2.087.496.596.795 1.291.795 2.087 0 3.08-1.889 3.677-3.677 3.875.298.398.596.895.596 1.59v2.187c0 .198.1.497.596.397C13.714 14.41 16 11.43 16 7.95 15.9 3.578 12.323 0 7.95 0z" />
                               </svg>
                               <span className="flex-auto pl-16 pr-8 -ml-16">Continue with GitHub</span>
@@ -93,7 +122,6 @@ export default function SignIn() {
                                 </button>
                             </div>
                         </div>
-                        </form>
                         <div className="text-gray-600 text-center mt-6">
                           Don't you have an account? <Link href="/signup" className="text-blue-600 hover:underline transition duration-150 ease-in-out">Sign up</Link>
                         </div>
