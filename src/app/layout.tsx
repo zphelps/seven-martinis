cf46996bb1c8f92362cb91de46f4d482999b7e52
benderd <davidwolfebender@gmail.com>
2025-10-13 12:59:26 -0400

    Made attempt to fix bug.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers";
 import { config } from "@/config";
@@ -9,35 +9,34 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({
-    subsets: ['latin'],
-    variable: '--font-inter',
-})
-import { Cormorant_Garamond } from 'next/font/google'
+    subsets: ["latin"],
+    variable: "--font-inter",
+});
 
 const cormorant = Cormorant_Garamond({
-    subsets: ['latin'],
-    weight: ['400', '600'], // adjust as needed
-    variable: '--font-cormorant',
-})
+    subsets: ["latin"],
+    weight: ["400", "600"],
+    variable: "--font-cormorant",
+});
 
 export const metadata: Metadata = getMetadata();
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" className={`${inter.variable} ${cormorant.variable}`}>
-      <meta name="viewport" content="width=device-width, user-scalable=no" />
-      <body className={cn(inter.className)}>
+                                       children,
+                                   }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en" className={`${inter.variable} ${cormorant.variable}`}>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <body className={cn("font-inter bg-background text-foreground")}>
         <Suspense fallback={<p>Loading...</p>}>
-          <Providers>
-            {children}
-            <Toaster />
-          </Providers>
+            <Providers>
+                {children}
+                <Toaster />
+            </Providers>
         </Suspense>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
